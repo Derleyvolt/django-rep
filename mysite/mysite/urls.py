8c6d5b4606f8076a5c8b017bf79c8ffd408e34c5
Derleyvolt <100palavr@gmail.com>
2022-12-26 12:46:24 -0300

    account

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 # from rest_framework.authtoken.views import obtain_auth_token 
 from rest_framework_simplejwt import views as jwt_views
 
+# teste
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('planilha.urls')),
