cb737e8493f8ba85bb4c4d3f76696775cb39527f
Wanderley <100Palavr@gmail.com>
2022-12-31 10:36:47 -0300

    Add files via upload

--- mysite/planilha/urls.py
+++ mysite/planilha/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token 
-from .views import PlanilhaView
+from .views import PlanilhaViewset
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'planilha', PlanilhaViewset)
 
 urlpatterns = [
-    path('planilha', PlanilhaView.as_view({'get' : 'listar', 'post' : 'atualizar'})),
-    path('planilha/<str:nome>', PlanilhaView.as_view({'get' : 'retrieve_by_name'})),
-    path('planilha/<int:salary>', PlanilhaView.as_view({'get' : 'retrieve_by_salary'})),
+    path('', include(router.urls))
+    # path('planilha', PlanilhaView.as_view({'get' : 'listar', 'post' : 'atualizar'})),
+    # path('planilha/<str:nome>', PlanilhaView.as_view({'get' : 'retrieve_by_name'})),
+    # path('planilha/<int:salary>', PlanilhaView.as_view({'get' : 'retrieve_by_salary'})),
 ]
